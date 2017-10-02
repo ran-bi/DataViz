1f7c68e029f77fa1a064aefd1e70fa5b747aba80
ran-bi <rbi@uchicago.edu>
2017-10-02 10:54:09 -0500

    merge data

--- PA1.R
+++ PA1.R
@@ -32,3 +32,13 @@ acc <- bind_rows(acc2014, acc2015)
 count(acc, RUR_URB)
 #NA Values: Because RUR_URB variable is missing from acc2014. 
 #The number of NA equals to the number of observations in acc2014.
+
+#Mergingo n another data source
+fips <- read_csv("fips.csv")
+glimpse(fips)
+acc$STATE <- as.character(acc$STATE)
+acc$COUNTY <- as.character(acc$COUNTY)
+acc$STATE <- str_pad(acc$STATE, 2, "left", "0")
+acc$COUNTY <- str_pad(acc$COUNTY, 3, "left", "0")
+acc <- plyr::rename(acc, c("STATE" = "StateFIPSCode", "COUNTY" = "CountyFIPSCode"))
+acc %>% left_join(fips)
