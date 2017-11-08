844a22b7958ca27fc7d407e8fd8bad3f72196f95
ran-bi <rbi@uchicago.edu>
2017-11-08 17:19:51 -0600

    set spacing guidelines

---
+++ d3.js-intro/script.js
@@ -0,0 +1,6 @@
+// Set Spacing Guidelines
+var margin = {top: 125, right: 75, bottom: 100, left: 100} 
+var width = 800 - margin.left - margin.right;
+var height = 800 - margin.top - margin.bottom;
+var barPadding = 20;
+var colors = ["#FDBF11", "#55A7ED"];
