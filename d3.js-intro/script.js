4fb6cf54f66d064e39f509bd89998c22312486b7
ran-bi <rbi@uchicago.edu>
2017-11-08 17:26:05 -0600

    add avg and g

--- d3.js-intro/script.js
+++ d3.js-intro/script.js
@@ -4,3 +4,13 @@ var width = 800 - margin.left - margin.right;
 var height = 800 - margin.top - margin.bottom;
 var barPadding = 20;
 var colors = ["#FDBF11", "#55A7ED"];
+
+// Create and size svg
+var svg = d3.select("#chart")
+	.append("svg")
+	.attr("width", width + margin.left + margin.right)
+	.attr("height", height + margin.top + margin.bottom);
+
+// Create and place start of SVG g tag, where bar chart will appear
+var g = svg.append("g")
+.attr("transform", "translate(" + margin.left + "," + margin.top + ")"); 
